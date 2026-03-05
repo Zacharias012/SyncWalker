a7314ec3c92f8a8b576c60f32476f705d39d0483
zwagner <Zacharias.Wagner@jambit.com>
2026-03-05 17:07:58 +0100

    add error handling for loading props file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,23 +3,34 @@ package org.example;
 import org.example.treeComponents.*;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Properties;
 
 public class Main {
+
+    public static String folderSource;
+    public static String folderTarget;
+
     public static void main(String[] args) throws Exception {
 
-        Properties props = new Properties();
-        props.load(new FileInputStream("config.properties"));
-        String folderSource = props.getProperty("folder_source");
-        String folderTarget = props.getProperty("folder_target");
+        try {
+            loadProperties();
+        } catch (Exception e){
+            System.out.println("No config.properties file found!");
+            return;
+        }
 
         TreeBuilder treeBuilderSource = new TreeBuilder(Paths.get(folderSource));
         //treeBuilderSource.printTree();
-
         TreeBuilder treeBuilderTarget = new TreeBuilder(Paths.get(folderTarget));
         //treeBuilderTarget.printTree();
+    }
 
-
+    public static void loadProperties() throws IOException {
+        Properties props = new Properties();
+        props.load(new FileInputStream("config.properties"));
+        folderSource = props.getProperty("folder_source");
+        folderTarget = props.getProperty("folder_target");
     }
 }
